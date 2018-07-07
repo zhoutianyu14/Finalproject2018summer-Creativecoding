627632e76671ba8df5b99ccd65ffe16ec816ce08
天宇 <tianyu2014@sjtu.edu.cn>
2018-07-07 19:45:27 +0800

    Add files via upload

---
+++ 声波雷达arduino/sketch_jul06b/sketch_jul06b.ino
@@ -0,0 +1,58 @@
+
+#include<Servo.h>. 
+const int trigPin = 10;
+const int echoPin = 11;
+
+long duration;
+int distance;
+Servo myServo; 
+void setup() {
+pinMode(trigPin, OUTPUT); 
+pinMode(echoPin, INPUT); 
+  Serial.begin(9600);
+  myServo.attach(12);
+pinMode(8, OUTPUT); 
+}
+void loop() {
+
+for(int i=15;i<=165;i++){  
+  myServo.write(i);
+delay(30);
+  distance = calculateDistance();
+  Serial.print(i); 
+  Serial.print(","); 
+  Serial.print(distance); 
+  Serial.print("."); 
+if(distance<20){ 
+tone(8, 500, 100);
+  }else if(distance>20 && distance<40){
+tone(8, 200, 100);
+  }
+}
+
+for(int i=165;i>15;i--){  
+  myServo.write(i);
+delay(30);
+  distance = calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
+if(distance<20){ 
+tone(8, 500, 100);
+  }else if(distance>20 && distance<40){
+tone(8, 200, 100);
+  }
+  }
+}
+
+int calculateDistance(){ 
+digitalWrite(trigPin, LOW); 
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH); 
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); 
+  distance= duration*0.034/2;
+return distance;
+}
